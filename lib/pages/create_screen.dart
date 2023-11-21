87228a333ccf5ce0255316861cf356a7fc9c9aed
JoaoMarceloDMSilva <joaomarcelo.dnzmsilva@gmail.com>
2023-11-20 21:30:15 -0300

    Tela 02 Primeiro Contêiner

--- lib/pages/create_screen.dart
+++ lib/pages/create_screen.dart
@@ -9,23 +9,51 @@ class CreateScreen extends StatelessWidget {
       appBar: AppBar(
         backgroundColor: const Color(0xff2C2828),
         elevation: 0,
-        title: const Text("Nome App", style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
-        leading: Icon(
+        title: const Text("Nome App",
+            style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
+        leading: const Icon(
           Icons.arrow_back,
         ),
         actions: [
           Padding(
-            padding: const EdgeInsets.all(10.0), // Add padding around the button
+            padding:
+                const EdgeInsets.all(10.0), // Add padding around the button
             child: FloatingActionButton(
               backgroundColor: Colors.white,
               foregroundColor: Colors.black,
+              onPressed: () {},
+              shape: const RoundedRectangleBorder(
+                  borderRadius: BorderRadius.all(Radius.circular(12))),
               child: Text(
                 "Criar",
                 style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
               ),
-              onPressed: () {},
-              shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.all(Radius.circular(12))),
+            ),
+          )
+        ],
+      ),
+      body: Column(
+        children: [
+          Container(
+            padding: EdgeInsets.all(10),
+            margin: EdgeInsets.only(top: 15),
+            child: Column(
+              children: [
+                Text(
+                  "Bora ver se essa merda funciona",
+                  style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 25,
+                      fontWeight: FontWeight.bold),
+                ),
+                Text(
+                  "Já estou sem paciência",
+                  style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 15,
+                      fontWeight: FontWeight.normal),
+                )
+              ],
             ),
           )
         ],
@@ -33,4 +61,3 @@ class CreateScreen extends StatelessWidget {
     );
   }
 }
-
