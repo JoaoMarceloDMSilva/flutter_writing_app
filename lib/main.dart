18aa52f07be6aba7e4b367a05dd737a05b9d1b3b
JoaoMarceloDMSilva <joaomarcelo.dnzmsilva@gmail.com>
2023-11-25 17:49:27 -0300

    Tela Criar Notas Finalizada

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_writing_app/pages/create_new_note.dart';
 import 'package:flutter_writing_app/pages/principal_chapter.dart';
-// import 'package:flutter_writing_app/pages/create_screen.dart';
-//import 'package:flutter_writing_app/my_widgets/home_appbar.dart';
-// import 'package:flutter_writing_app/pages/initial_screen.dart';
+import 'package:flutter_writing_app/pages/create_screen.dart';
+import 'package:flutter_writing_app/my_widgets/home_appbar.dart';
+import 'package:flutter_writing_app/pages/initial_screen.dart';
 
 void main(List<String> args) {
   runApp(const MyApp());
@@ -14,10 +14,10 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      // home: InitialScreen(),
-      // home: CreateScreen(),
-      // home: PrincipalChapter(),
-      home: CreateNote(),
+      // home: InitialScreen(), //Tela 01
+      // home: CreateScreen(), // Tela 02
+      // home: PrincipalChapter(), // Tela 03
+      home: CreateNote(), //Tela 04
     );
   }
 }
--- lib/pages/create_new_note.dart
+++ lib/pages/create_new_note.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 class CreateNote extends StatelessWidget {
-  const CreateNote({super.key});
+  const CreateNote({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -24,39 +24,87 @@ class CreateNote extends StatelessWidget {
             child: const Text(
               "Salvar",
               style: TextStyle(
-                  fontSize: 16,
-                  fontWeight: FontWeight.bold,
-                  color: Colors.white),
+                fontSize: 16,
+                fontWeight: FontWeight.bold,
+                color: Colors.white,
+              ),
             ),
           ),
         ],
       ),
-      body: Column(
-        children: [
-          Container(
-            // CAIXA TEXTO INPUT TITULO
-            padding: const EdgeInsets.all(12),
-            // margin: EdgeInsets.only(top: 5),
-            child: TextField( // TÍTULO
-              decoration: InputDecoration(
-                hintText: "Título",
-                border: OutlineInputBorder(),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Container(
+              // CAIXA TEXTO INPUT TITULO
+              padding: const EdgeInsets.all(12),
+              child: TextField(
+                // TÍTULO
+                decoration: InputDecoration(
+                  hintText: "Título",
+                  border: OutlineInputBorder(),
+                ),
               ),
             ),
-          ),
-          Container(
-            // CAIXA TEXTO INPUT TEXTO
-            padding: const EdgeInsets.all(12),
-            // margin: EdgeInsets.only(top: 5),
-            child: TextField(
-              decoration: InputDecoration(
-                hintText: "Texto",
-                border: OutlineInputBorder(),
+            Container(
+              // CAIXA TEXTO INPUT TEXTO
+              padding: const EdgeInsets.all(12),
+              child: TextField(
+                decoration: InputDecoration(
+                  hintText: "Texto",
+                  border: OutlineInputBorder(),
+                ),
+                maxLines: null,
               ),
-              maxLines: null, // ou maxLines: true,
             ),
-          ),
-        ],
+            Container(
+              // TEXTO: Imagem e botão "+"
+              margin: const EdgeInsets.symmetric(horizontal: 12, vertical: 8),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  const Text(
+                    "Imagem",
+                    style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+                  ),
+                  TextButton(
+                    onPressed: () {
+                      // Lógica Aqui
+                    },
+                    child: const Icon(
+                      Icons.add,
+                      size: 24,
+                      color: Colors.black, // Cor do ícone
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Container(
+              margin: const EdgeInsets.symmetric(horizontal: 12),
+              height: 1,
+              color: Colors.black, // Cor da linha
+            ),
+            Container(
+              margin: const EdgeInsets.only(left: 12, top: 10),
+              width: 100,
+              height: 100,
+              decoration: BoxDecoration(
+                border: Border.all(
+                  color: Colors.black, // Cor da borda
+                  width: 1, // Largura da borda
+                ),
+              ),
+              child: Center(
+                child: Icon(
+                  Icons.image, // Ícone padrão de imagem
+                  size: 25,
+                  color: Colors.black, // Cor do ícone
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
