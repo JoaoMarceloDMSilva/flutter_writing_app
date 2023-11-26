5122ab9f63c4ba8a41d7e9448336a6fc6b21c93f
JoaoMarceloDMSilva <joaomarcelo.dnzmsilva@gmail.com>
2023-11-26 14:29:05 -0300

    Pagina Editar Historia

--- lib/pages/edit_page.dart
+++ lib/pages/edit_page.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_writing_app/my_widgets/tab_bar_add.dart';
 
 class EditPage extends StatelessWidget {
-  const EditPage({super.key});
+  const EditPage({Key? key});
 
   @override
   Widget build(BuildContext context) {
@@ -33,11 +33,81 @@ class EditPage extends StatelessWidget {
           ),
         ],
       ),
-      body: Column(
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            buildItem(),
+            buildCategoryAddItem(chapterTitle: "Personagens", itemName: "Nome Personagem"),
+            buildCategoryAddItem(chapterTitle: "Capítulos", itemName: "Nome Capítulo"),
+            buildCategoryAddItem(chapterTitle: "Notas", itemName: "Nome Nota"),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget buildItem() {
+    return Container(
+      padding: EdgeInsets.all(12),
+      child: Row(
         children: [
-          // CategoryAdd(chapterTitle: "Personagens"),
+          Container(
+            width: 100,
+            height: 100,
+            decoration: BoxDecoration(
+              shape: BoxShape.circle,
+              color: Colors.grey,
+            ),
+            child: Icon(
+              Icons.add_photo_alternate, 
+              size: 50,
+              color: Colors.black,
+            ),
+          ),
+          SizedBox(width: 6),
+          Expanded(
+            child: TextFormField(
+              decoration: InputDecoration(
+                hintText: "Título",
+                border: OutlineInputBorder(),
+              ),
+            ),
+          ),
         ],
       ),
     );
   }
+}
+
+Widget buildCategoryAddItem({required String chapterTitle, required String itemName}) {
+  return Container(
+    padding: EdgeInsets.all(16),
+    child: Column(
+      children: [
+        CategoryAdd(chapterTitle: chapterTitle),
+        Row(
+          children: [
+            Expanded(
+              child: Container(
+                padding: EdgeInsets.all(12),
+                color: Colors.grey,
+                child: Text(
+                  itemName,
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+            ),
+            IconButton(
+              icon: Icon(Icons.delete),
+              onPressed: () {
+                // Adicione a lógica para excluir aqui
+              },
+            ),
+          ],
+        ),
+      ],
+    ),
+  );
 }
